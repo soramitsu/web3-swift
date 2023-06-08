c9cb6285532309aa68fd1549181e6df9f4ad1f9b
Alex Lebedko <lebedko@soramitsu.co.jp>
2023-06-08 10:47:01 +0700

    remove bigint & secp256k1 dependencies

--- Package.swift
+++ Package.swift
@@ -25,9 +25,7 @@ let package = Package(
     ],
     dependencies: [
         // Core dependencies
-        .package(url: "https://github.com/attaswift/BigInt.git", from: "5.3.0"),
         .package(url: "https://github.com/krzyzanowskim/CryptoSwift.git", from: "1.6.0"),
-        .package(name: "secp256k1", url: "https://github.com/Boilertalk/secp256k1.swift.git", from: "0.1.7"),
         .package(url: "https://github.com/apple/swift-collections.git", .upToNextMajor(from: "1.0.3")),
         .package(url: "https://github.com/vapor/websocket-kit", .upToNextMajor(from: "2.6.1")),
 
@@ -42,9 +40,7 @@ let package = Package(
         .target(
             name: "Web3",
             dependencies: [
-                .product(name: "BigInt", package: "BigInt"),
                 .product(name: "CryptoSwift", package: "CryptoSwift"),
-                .product(name: "secp256k1", package: "secp256k1"),
                 .product(name: "Collections", package: "swift-collections"),
                 .product(name: "WebSocketKit", package: "websocket-kit"),
             ],
@@ -63,7 +59,6 @@ let package = Package(
             name: "Web3ContractABI",
             dependencies: [
                 .target(name: "Web3"),
-                .product(name: "BigInt", package: "BigInt"),
                 .product(name: "CryptoSwift", package: "CryptoSwift"),
             ],
             path: "Sources",
